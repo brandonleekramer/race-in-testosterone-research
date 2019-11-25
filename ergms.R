30f1e035cf43c70023a2bad88928342a000c36cc
Brandon Kramer <brandonleekramer@gmail.com>
2019-11-24 20:03:17 -0500

    Fit attribute-based ERGMs.

--- ergms.R
+++ ergms.R
@@ -6,7 +6,7 @@
 rm(list = ls())
 knitr::opts_knit$set(root.dir = "C:/Users/bkram/CloudStation/git/racialization-in-testosterone-research")
 for (pkg in c("tidyverse", "igraph", "googlesheets", "networkD3", 
-              "ergm", "sna", "statnet", "btergm", "parallel")) {library(pkg, character.only = TRUE)}
+              "ergm", "sna", "statnet", "parallel")) {library(pkg, character.only = TRUE)}
 
 # loading data 
 setwd("C:/Users/bkram/CloudStation/git/data")
@@ -27,66 +27,279 @@ network.size(tnet) # 309
 network.edgecount(tnet) #595
 network.dyadcount(tnet) #95172
 
-# applying nodal attributes to network 
+# applying nodal attributes to the network 
 
-tnet%v%"evidence" <- nodelist$evidence 
+# article attributes 
 tnet%v%"year" <- nodelist$year
-tnet%v%"groupcode" <- nodelist$groupcode
+tnet%v%"evidence" <- nodelist$evidence 
+tnet%v%"outcome" <- nodelist$outcomecode
+tnet%v%"domain" <- nodelist$domaincode
+tnet%v%"journal" <- nodelist$journalcode
+tnet%v%"timescited" <- nodelist$gstimescited
+tnet%v%"region" <- nodelist$region
+tnet%v%"drd" <- nodelist$datarecyclingbinary
+tnet%v%"drm" <- nodelist$datarecyclingmulti
+# population difference tests 
+tnet%v%"group" <- nodelist$groupcode 
+tnet%v%"testedmen" <- nodelist$testedmen 
 tnet%v%"rttcode" <- nodelist$rttcode
-tnet%v%"gstimescited" <- nodelist$gstimescited
+tnet%v%"wbcomp" <- nodelist$wbcompcode
+tnet%v%"bacomp" <- nodelist$bacompcode
+tnet%v%"wacomp" <- nodelist$wacompcode
+tnet%v%"hisp" <- nodelist$hisp
+tnet%v%"wvsnw" <- nodelist$wvsnw
+# control tests 
+tnet%v%"timediff" <- nodelist$timediff
+tnet%v%"agemeasured" <- nodelist$agemeasured	
+tnet%v%"agecontrol" <- nodelist$agecontrol	
+tnet%v%"agediff" <- nodelist$agediff	
+tnet%v%"bmimeasured" <- nodelist$bmimeasured	
+tnet%v%"bmicontrolled" <- nodelist$bmicontrolled	
+tnet%v%"bmidiff" <- nodelist$bmidiff	
+tnet%v%"illnesscontrolled" <- nodelist$illnesscontrolled	
+tnet%v%"sescontrolled" <- nodelist$sescontrolled	
+tnet%v%"medscontrolled" <- nodelist$medscontrolled
 
-# BASELINE AND NODAL ATTRIBUTE TESTING 
+
+################################################### BASELINE AND STUDY-LEVEL ATTRIBUTES 
 
 # baseline (AIC: 6706    BIC: 6715)
 t <- ergm(tnet ~ edges, 
           control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
 summary(t)
 
-# evidence (AIC: 6166    BIC: 6185) 
-t_ev <- ergm(tnet ~ edges + nodematch("evidence"), 
-          control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
-summary(t_ev)
-
-# year (AIC: 6643    BIC: 6662)
+# year 
 t_yr <- ergm(tnet ~ edges + nodecov("year"),
              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
 summary(t_yr)
 
-# population grouping (AIC: 5899    BIC: 5947)  
-t_pop <- ergm(tnet ~ edges + nodematch("groupcode"), 
+# evidence 
+t_ev <- ergm(tnet ~ edges + nodefactor("evidence"), 
+          control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_ev)
+
+# outcome 
+t_oc <- ergm(tnet ~ edges + nodefactor("outcome"), 
              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
-summary(t_pop)
-# nodefactor works better here 
+summary(t_oc)
 
-# racial testosterone tested (AIC: 6651    BIC: 6670)    
-t_rtt <- ergm(tnet ~ edges + nodematch("rttcode"), 
+# domain
+t_dom <- ergm(tnet ~ edges + nodefactor("domain"), 
+                  control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom)
+
+# journal
+t_jnl <- ergm(tnet ~ edges + nodefactor("journal"), 
               control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
-summary(t_rtt)
+summary(t_jnl)
 
-# times cited  (AIC: 6707    BIC: 6726) # ns effect   
-t_rtt <- ergm(tnet ~ edges + nodecov("gstimescited"), 
+# times cited     
+t_tc <- ergm(tnet ~ edges + nodecov("gstimescited"), 
+                control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_tc)
+
+# region
+t_rgn <- ergm(tnet ~ edges + nodefactor("region"), 
               control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
-summary(t_rtt)
+summary(t_rgn)
+
+# data recycling (dummy)
+t_drd <- ergm(tnet ~ edges + nodefactor("drd"), 
+              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_drd)
+
+# data recycling (multi) 
+t_drm <- ergm(tnet ~ edges + nodefactor("drm"), 
+              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_drmlt)
+
+# domain, region, and data recycling had biggest initial impact 
+
+# domain + drd
+t_dom_drd <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drd"), 
+              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drd)
+
+# domain + drm
+t_dom_drm <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm"), 
+                  control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm)
+
+# domain,region
+t_dom_rgn <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("region"), 
+                      control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_rgn)
+
+# region,drm
+t_rgn_drm <- ergm(tnet ~ edges + nodefactor("region") + nodefactor("drm"), 
+                  control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_rgn_drm)
+
+# domain,drd,region = best combination so far 
+t_dom_drm_rgn <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region"), 
+                      control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn)
+
+# double-checking to see if nsig covariates matter again 
+
+# domain,drd,region,year
+t_dom_drm_rgn_yr <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + 
+                        nodefactor("region") + nodecov("year"), 
+                      control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_yr)
+
+# domain,drd,region,outcome
+t_dom_drm_rgn_oc <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + 
+                           nodefactor("region") + nodefactor("outcome"), 
+                         control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_oc)
+
+# domain,drd,region,journal
+t_dom_drm_rgn_jnl <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + 
+                           nodefactor("region") + nodefactor("journal"), 
+                         control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_jnl)
+
+####################################################################################################################
+# domain,drd,region = best combination so far 
+t_dom_drm_rgn <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region"), 
+                      control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn)
+gof_dom_drm_rgn <- gof(t_dom_drm_rgn) 
+plot(gof_dom_drm_rgn); gof_dom_drm_rgn
+# GOF only at 0.6457251
+####################################################################################################################
+
+################################################### SPECIFIC FORMS OF RACIAL DIFFERENCE TESTING 
+
+# tested_men  
+t_dom_drm_rgn_men <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                           nodefactor("testedmen"),
+                         control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_men)
+
+# population grouping 
+t_dom_drm_rgn_gp <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                      nodefactor("group"),
+                      control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_gp)
+
+# racial testosterone tested     
+t_dom_drm_rgn_gp_rtt <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                           nodefactor("group") + nodematch("rttcode"),
+                         control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_gp_rtt)
+
+# white-black differences 
+t_dom_drm_rgn_gp_wb <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                               nodefactor("group") + nodematch("wbcomp"),
+                             control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_gp_wb)
+
+# white-asian differences
+t_dom_drm_rgn_gp_wa <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                              nodefactor("group") + nodematch("wacomp"),
+                            control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_gp_wa)
+
+# black-asian differences
+t_dom_drm_rgn_gp_ba <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                              nodefactor("group") + nodematch("bacomp"),
+                            control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_gp_ba)
+
+# black-asian differences
+t_dom_drm_rgn_gp_hsp <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                              nodefactor("group") + nodematch("hisp"),
+                            control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_gp_hsp)
+
+# western-nonwestern comps 
+t_dom_drm_rgn_gp_wvsnw <- ergm(tnet ~ edges + nodefactor("domain") + nodefactor("drm") + nodefactor("region") + 
+                               nodefactor("group") + nodematch("wvsnw"),
+                             control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_gp_wvsnw)
+
+
+################################################### PRELIMINARY STRUCTURAL FACTORS 
+
+# baseline (AIC: 6706    BIC: 6715)
+t <- ergm(tnet ~ edges, 
+          control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t)
+
+# controlling for isolates
+t_iso <- ergm(tnet ~ edges + isolates, 
+          control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_iso)
+
+# controlling for isolates
+summary(tnet~edges + triangle)
+t_tri <- ergm(tnet ~ edges + triangle, 
+              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_tri)
+
+# looking at degree distributions 
+summary(tnet ~ edges + idegree(1:10))
+
+# degree = 1
+t_id1 <- ergm(tnet ~ edges + idegree(1), 
+              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_id1)
+
+# degree = 1:2
+t_id1_2 <- ergm(tnet ~ edges + idegree(1:2), 
+              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_id1_2)
+
+# degree = 1 + iso
+t_id1_iso <- ergm(tnet ~ edges + idegree(1) + isolates, 
+              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_id1_iso)
+
+# adding in isolates + degree term  
+t_dom_drm_rgn_iso_dg1 <- ergm(tnet ~ edges + nodematch("domain") + nodefactor("drm") + nodefactor("region") +
+                                idegree(1) + isolates, 
+                              control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_dom_drm_rgn_iso_dg1)
+gof_dom_drm_rgn_iso_dg1 <- gof(t_dom_drm_rgn_iso_dg1) 
+plot(gof_dom_drm_rgn_iso_dg1); gof_dom_drm_rgn_iso_dg1
+
+# comparison with previous best model = slightly better but GOF still bad 
+gof_dom_drm_rgn <- gof(t_dom_drm_rgn) 
+plot(gof_dom_drm_rgn); gof_dom_drm_rgn
+
+# gwesp(0.25) = model degeneracy
+t_gw25 <- ergm(tnet ~ edges + gwesp(0.25),
+                  control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_gw25)
+
+# gwesp(0.50) = model degeneracy
+t_gw50 <- ergm(tnet ~ edges + gwesp(0.50),
+               control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_gw50)
+
+# gwesp(0.10) = model degeneracy
+t_gw10 <- ergm(tnet ~ edges + gwesp(0.10),
+               control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_gw10)
+
+# gwesp(0.01) = model degeneracy
+t_gw01 <- ergm(tnet ~ edges + gwesp(0.01),
+               control=control.ergm(parallel=8, MCMLE.maxit = 25, parallel.type="PSOCK"))
+summary(t_gw01)
+
+
+
+# must finish coding slippage variable ######################
+
+
+references 
+
+https://statnet.org/trac/raw-attachment/wiki/Sunbelt2016/ergm_tutorial.html
+
+http://statnet.csde.washington.edu/workshops/EUSN/current/ergm/ergm_tutorial.html
 
 
 
 
-# PRELIMINARY STRUCTURAL FACTORS 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# dont test slippage (must finish coding)
